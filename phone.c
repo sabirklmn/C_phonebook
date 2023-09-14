19feec21a4bc0a8c6958e9b58506dcc3380bf40b
Sabir <sabirn538@gmail.com>
2023-09-14 18:41:37 +0530

    structure created

---
+++ phone.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+struct person{
+    char name[30];
+    char country_code[4];
+    long int moble_no;
+    char sex[8];
+    char mail[100];
+};
