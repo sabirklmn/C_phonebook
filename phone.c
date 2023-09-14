08bc715592528bfd3b740e57b07ed3fa5bc39bf0
Sabir <sabirn538@gmail.com>
2023-09-14 19:13:48 +0530

    created menu structure

--- phone.c
+++ phone.c
@@ -14,7 +14,7 @@ struct person{
 
 typedef struct person person;
 
-
+void print_menu();
 void remove_all();
 void print_all();
 void add_person();
@@ -23,3 +23,35 @@ void serach_person();
 void remove_person();
 void update_person();
 void take_input(person *p);
+
+
+int main ()
+{
+    start();
+    return 0;
+} 
+
+void print_menu()
+{
+    system("clear");
+    printf("\t\t\************************************\n");
+    printf("*       Welcome to my phone book         *\n");
+    printf("\t\t\************************************\n");
+    printf("\t\t\t1) list record \n\n");
+    printf("\t\t\t2) Add person \n\n");
+    printf("\t\t\t3) Search person Details \n\n");
+    printf("\t\t\t4) Remove person\n\n");
+    printf("\t\t\t5) update person \n\n");
+    printf("\t\t\t6) Delete all contacts \n\n");
+    printf("\t\t\t7) exit phonebbok \n\n");
+
+    printf("\t\t\t\t Enter your Choice : ");
+}
+
+
+
+
+
+
+
+
