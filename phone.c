76f27e142c781d635d492a9e4b1c0b0b7e977fb5
Sabir <sabirn538@gmail.com>
2023-09-14 18:59:48 +0530

    functions name created

--- phone.c
+++ phone.c
@@ -10,3 +10,16 @@ struct person{
     char sex[8];
     char mail[100];
 };
+
+
+typedef struct person person;
+
+
+void remove_all();
+void print_all();
+void add_person();
+void list_record();
+void serach_person();
+void remove_person();
+void update_person();
+void take_input(person *p);
